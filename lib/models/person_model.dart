0550e67fa5f0c8504651596588da55aacfcb0ca3
HokulaniTopping <hokulanigit@gmail.com>
2026-01-28 16:49:01 -0800

    idk

--- lib/models/person_model.dart
+++ lib/models/person_model.dart
@@ -1,10 +1,11 @@
 class PersonModel {
   final String name;
   final String phoneNumber;
+  final int daysCheckedIn;
 
   PersonModel({
     required this.name,
     required this.phoneNumber,
+    this.daysCheckedIn = 0,
   });
 }
-
